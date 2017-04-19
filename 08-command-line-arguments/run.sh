1df83644b152fcda938195ed48f4a2ea2d82fac8
nipa <nipa@codefx.org>
2017-04-19 19:21:31 +0200

    [08] Solve

--- 08-command-line-arguments/run.sh
+++ 08-command-line-arguments/run.sh
@@ -9,4 +9,8 @@ echo " > run monitor"
 echo ""
 
 $JAVA --module-path libs:mods \
+	--add-modules monitor.rest \
+	--add-reads monitor=monitor.rest \
+	--add-exports monitor.rest/monitor.rest=monitor \
+	--add-opens monitor.persistence/monitor.persistence.dtos=hibernate \
 	--module monitor
